245d03cd89a4dc8215c88dce02f43be3b8a36a25
Sebastián Ramírez Méndez <u20191e575@upc.edu.pe>
2025-01-15 19:47:18 -0500

    fix: phone number fixed.

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -7,7 +7,7 @@ import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 const WhatsAppButton: React.FC = () => {
   return (
     <a
-      href="https://wa.me/51980731766?text=¡Hola!%20Quiero%20más%20información%20sobre%20sus%20productos."
+      href="https://wa.me/51975885868?text=¡Hola!%20Quiero%20más%20información%20sobre%20sus%20productos."
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-4 right-4 bg-green-500 text-white flex items-center gap-2 px-4 py-3 rounded-full shadow-lg hover:bg-green-600 transition-all"
